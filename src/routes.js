e0873cbb74c3920b1d4c02afd03bb74f50fcd9ec
João Pedro Beck Land <joaopedrobeckland@gmail.com>
2019-04-27 12:42:04 -0300

    marketplace

--- src/routes.js
+++ src/routes.js
@@ -4,11 +4,18 @@ const routes = express.Router()
 
 const authMiddleware = require('./app/middlewares/auth')
 
-const UserController = require('./app/controller/UserController')
-const SessionController = require('./app/controller/SessionController')
+const controllers = require('./app/controllers')
 
-routes.post('/users', UserController.store)
-routes.post('/sessions', SessionController.store)
-routes.get('/teste', authMiddleware, (req, res) => res.json({ ok: true }))
+/* Users */
+routes.post('/users', controllers.UserController.store)
+routes.post('/sessions', controllers.SessionController.store)
+
+routes.use(authMiddleware)
+/* Ads */
+routes.get('/ads', controllers.AdController.index)
+routes.get('/ads/:id', controllers.AdController.show)
+routes.post('/ads', controllers.AdController.store)
+routes.put('/ads/:id', controllers.AdController.uptade)
+routes.delete('/ads/:id', controllers.AdController.destroy)
 
 module.exports = routes
