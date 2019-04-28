d1f10bf4f67dda03afcf7fbebfa621c6c1a53644
João Pedro Beck Land <joaopedrobeckland@gmail.com>
2019-04-27 22:27:06 -0300

    start email

---
+++ src/config/mail.js
@@ -0,0 +1,9 @@
+module.exports = {
+  host: 'smtp.mailtrap.io',
+  port: 2525,
+  secure: false,
+  auth: {
+    user: 'fe427000f11281',
+    pass: 'be33cacb65036b'
+  }
+}
--- src/routes.js
+++ src/routes.js
@@ -15,7 +15,10 @@ routes.use(authMiddleware)
 routes.get('/ads', controllers.AdController.index)
 routes.get('/ads/:id', controllers.AdController.show)
 routes.post('/ads', controllers.AdController.store)
-routes.put('/ads/:id', controllers.AdController.uptade)
+routes.put('/ads/:id', controllers.AdController.update)
 routes.delete('/ads/:id', controllers.AdController.destroy)
+
+/* Purchase */
+routes.post('/purchases', controllers.PurchaseController.store)
 
 module.exports = routes
